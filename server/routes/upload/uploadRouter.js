a82cf755845c5022f76a0b2a696bd5c4ff592577
Aditya <adityaajmera060401@gmail.com>
2020-09-27 03:00:17 +0530

    Update Video Upload
    Additional: Change Folder Structure to Increase Efficiency

--- server/routes/upload/uploadRouter.js
+++ server/routes/upload/uploadRouter.js
@@ -1,33 +1,58 @@
 // import dependancies
-const express = require("express");
+const express = require('express');
+const multer = require('multer');
+
+var authenticate = require('../authenticate');
 const router = express.Router();
-const multer = require("multer");
 
 // multer middleware -- (Subject to reloactaion)
 
-// specifying path of video storage
+// Dealing with video storage on server side
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "media/uploads/");
+  // specifying path of file where video will be saved
+  destination: (req, file, cb) => {
+    cb(null, '../../uploads/');
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname.replace(/ /g, "_"));
+  // making sure file uploaded is a .mp4 file
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.mp4') {
+      return cb(res.status(400).end('Only .Mp4 Allowed'), false);
+    }
+    cb(null, true);
+  },
+  filename: (req, file, cb) => {
+    // replacing blank spaces in teh file name
+    cb(null, file.originalname.replace(/ /g, '_'));
   },
 });
 
 // handling constraints
 const upload = multer({
   storage: storage,
+  // file size limit to 200mb temporarily
   limits: {
     fileSize: 1024 * 1024 * 200, // 200 MB
   },
-});
+}).single('file');
 
 // port which receives the video
-router.post("/", upload.single("file"), (req, res, next) => {
-  res.status(200).json({
-    message: "Video upload successful",
-  });
-});
+router.post(
+  '/',
+  // only admin/proffessor can make videos
+  authenticate.verifyAdmin,
+  (req, res, next) => {
+    upload(req, res, (err) => {
+      if (err) {
+        return res.json({ success: false, err });
+      }
+      return res.json({
+        succes: true,
+        filePath: res.req.file.path,
+        fileName: res.req.file.fileName,
+      });
+    });
+  }
+);
 
 module.exports = router;
